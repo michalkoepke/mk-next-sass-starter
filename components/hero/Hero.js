de88eb384f5eb466d42cfc029f9e570a728f3e56
michalkoepke <michalkoepke@gmail.com>
2022-01-15 15:42:40 +0100

    first simple gsap animation added in hero

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
 
 import styles from "./Hero.module.scss";
 
 const Hero = () => {
+  //refs
+
+  // const tl = useRef(null);
+  const heroText = useRef(null);
+
+  //useEffect
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      defaults: { duration: 1.2, ease: "power3.inOut" },
+    });
+
+    tl.from(heroText.current, { opacity: 0, y: -100, stagger: 0.5 }).to(
+      heroText.current,
+      {
+        opacity: 1,
+        y: 0,
+      }
+    );
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.gridHero}>
-          <div className={styles.left}>
+          <div className={styles.left} ref={heroText}>
             <h1>Some Main Title</h1>
             <h2>Subtitle goes here</h2>
             <div className={styles.description}>
@@ -19,10 +41,7 @@ const Hero = () => {
             <button className={styles.buttonStandart}>CTA Button</button>
           </div>
 
-          <div className={styles.right}>
-            {/* <img src="https://picsum.photos/400/500" /> */}
-            {/* <p>Lorem ipsum dolor</p> */}
-          </div>
+          <div className={styles.right}></div>
         </div>
       </div>
     </div>
